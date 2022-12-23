081423e78ee2f9746cf340f79f0b9b3f5cf69acb
David <8039876+AmoebaProtozoa@users.noreply.github.com>
2022-12-23 10:39:27 +0800

    add tiflash compute node label compatibility (#168) (#174)
    Signed-off-by: David <8039876+AmoebaProtozoa@users.noreply.github.com>

--- util/engine/engine.go
+++ util/engine/engine.go
@@ -15,13 +15,15 @@
 package engine
 
 import (
+	"strings"
+
 	"github.com/pingcap/kvproto/pkg/metapb"
 )
 
 // IsTiFlash tests whether the store is based on tiflash engine.
 func IsTiFlash(store *metapb.Store) bool {
 	for _, label := range store.Labels {
-		if label.Key == "engine" && label.Value == "tiflash" {
+		if label.Key == "engine" && strings.Contains(label.Value, "tiflash") {
 			return true
 		}
 	}
